496aa3c358b5a6a1c249841c0c0196ad90dac90a
Criptograma-1 <andresa.info@gmail.com>
2021-04-18 18:53:17 +0000

    Fix identation task 2

--- 0x00-python-hello_world/2-positive_or_negative.py
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number == 0:
-print("{} is zero".format(number))
+ print("{0:d} is zero".format(number))
 elif number > 0:
-print("{} is positive".format(number))
-else:
-print("{} is negative".format(number))
+ print("{0:d} is positive".format(number))
+elif number < 0:
+ print("{0:d} is negative".format(number))
