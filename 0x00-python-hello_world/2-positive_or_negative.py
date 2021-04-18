b9348b2a41078939b9aeb2a222a5399ed89eb5a4
Criptograma-1 <andresa.info@gmail.com>
2021-04-18 18:41:29 +0000

    Fix PEP8 in task 2

--- 0x00-python-hello_world/2-positive_or_negative.py
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number == 0:
-	print("{} is zero".format(number))
+print("{} is zero".format(number))
 elif number > 0:
-	print("{} is positive".format(number))
+print("{} is positive".format(number))
 else:
-	print("{} is negative".format(number))
+print("{} is negative".format(number))
