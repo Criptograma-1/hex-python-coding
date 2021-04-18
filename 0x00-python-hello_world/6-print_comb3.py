9a35d0db2f011dbc0ae10559f46fc06b38412d42
Criptograma-1 <andresa.info@gmail.com>
2021-04-18 22:18:09 +0000

    Fix PEP8

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for x in range(10):
         if i <= x and i != x:
-            if  i == 8 and x == 9:
+            if i == 8 and x == 9:
                 print("{:d}".format(89))
             else:
                 print("{:d}{:d}".format(i, x), end=', ')
