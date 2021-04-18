234d9a8d54cf4ee6a06bff9f2aa5db085c319b82
Criptograma-1 <andresa.info@gmail.com>
2021-04-18 19:51:22 +0000

    Fix 3-last_digit.py

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = int(repr(number)[-1])
+if number > 0:
+    last = number%10
+elif number < 0:
+    last = -(abs(number)%10)
 if last > 5:
     print("last digit of {:d} is {:d} and is greater than 5".format(number, last))
 elif last == 0:
