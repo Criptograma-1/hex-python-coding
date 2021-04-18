5bfd1a6cf31279a2c80eec12f1f2a36491462a95
Criptograma-1 <andresa.info@gmail.com>
2021-04-18 22:14:27 +0000

    Add 6-print_comb3.py

---
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for x in range(10):
+        if i <= x and i != x:
+            if  i == 8 and x == 9:
+                print("{:d}".format(89))
+            print("{:d}{:d}".format(i, x), end=', ')
