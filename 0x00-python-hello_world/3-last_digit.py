66f92cc54752d9085287c2d89b57522f2425eb1b
Criptograma-1 <andresa.info@gmail.com>
2021-04-18 19:59:06 +0000

    fix 3-last_digit.py 2

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
-    last = number%10
-elif number < 0:
-    last = -(abs(number)%10)
+last = abs(number) % 10
+if number < 0:
+    last *= -1
 if last > 5:
-    print("last digit of {:d} is {:d} and is greater than 5".format(number, last))
+    print("last digit of {:d} is {:d} and is greater than 5".format(number, int(last)))
 elif last == 0:
-    print("last digit of {:d} is {:d} and is 0".format(number, last))
-elif last < 6:
-    print("last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last))
+    print("last digit of {:d} is {:d} and is 0".format(number, int(last)))
+else last < 0:
+    print("last digit of {:d} is {:d} and is less than 6 and not 0".format(number, int(last)))
