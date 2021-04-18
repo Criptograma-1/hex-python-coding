ea58b0b581a188b0884c2d290c31197a5b6c3e7d
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-04-18 17:00:35 -0300

    Fix PEP8

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -8,5 +8,5 @@ if last > 5:
     print("last digit of {:d} is {:d} and is greater than 5".format(number, int(last)))
 elif last == 0:
     print("last digit of {:d} is {:d} and is 0".format(number, int(last)))
-else last < 0:
+else:
     print("last digit of {:d} is {:d} and is less than 6 and not 0".format(number, int(last)))
