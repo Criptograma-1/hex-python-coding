3662b3349e53de7c2f79bf5fa700c429536a5a1c
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-04-18 15:54:45 -0300

    Fix PEP8

--- 0x00-python-hello_world/2-positive_or_negative.py
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number == 0:
- print("{0:d} is zero".format(number))
+ print("{} is zero".format(number))
 elif number > 0:
- print("{0:d} is positive".format(number))
+ print("{} is positive".format(number))
 elif number < 0:
- print("{0:d} is negative".format(number))
+ print("{} is negative".format(number))
