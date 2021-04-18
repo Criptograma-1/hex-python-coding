25a5da74e0148ce31923034b87787a91d7561a7e
Criptograma-1 <andresa.info@gmail.com>
2021-04-18 22:17:02 +0000

    Fix 6-print_comb3.py

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -4,4 +4,5 @@ for i in range(10):
         if i <= x and i != x:
             if  i == 8 and x == 9:
                 print("{:d}".format(89))
-            print("{:d}{:d}".format(i, x), end=', ')
+            else:
+                print("{:d}{:d}".format(i, x), end=', ')
