07f39973e91c1f6c96a1d92ecbc3b2bd39092e7d
Criptograma-1 <andresa.info@gmail.com>
2021-04-21 17:17:55 +0000

    Fix  0-square_matrix_simple.py

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,7 @@
 def square_matrix_simple(matrix=[]):
     if matrix is None:
         return None
-    new = [row[:] for row in matrix]
-    new = [cow[:] for cow in matrix]
+    new = [row[:] for row in matrix][cow[:] for cow in matrix]
     for row in range(len(new)):
         for col in range(len(new)):
             new[row][col] *= new[row][col]
