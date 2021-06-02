eff11a4c5ac39738f102fb022261a4533e3103a1
Criptograma-1 <andresa.info@gmail.com>
2021-06-02 01:45:43 +0000

    Add task 2

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Creating a class Square"""
+
+
+class Square:
+    """Derives a square"""
+    def __init__(self, size=0):
+        """initializes attributes
+        Args:
+            size (int): size of the square
+        Note:
+            ``Args`` section don't include `self` parameter
+        Raises:
+            TypeError: if `size` isn't an integer
+            ValueError: if `size` is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of a square
+        Returns:
+            the area of the square
+        """
+        return (self.__size ** 2)
