96da18e098dfba88e91d1eb5fcd71c7b06215b3d
Criptograma <81261737+Criptograma-1@users.noreply.github.com>
2021-06-01 22:37:19 -0300

    Fix ValueError msg

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -17,5 +17,5 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if (size < 0):
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
