3327f8e682c1accabc0b70938ff3ce15a592f34c
Criptograma-1 <andresa.info@gmail.com>
2021-06-03 01:32:28 +0000

    Add task 0

---
+++ 0x05-python-more_classes/0-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""defines class Rectangle"""
+
+    
+class Rectangle:
+    """Define the variable or attribute in the principal method"""
+    def __init__(self, width=0, height=0):
+        """initializes attributes
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        Note:
+            ``Args`` section don't include `self` parameter
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """property to retrive value of `width`"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """property setter method
+        Args:
+            value (int): value to be set
+        Raises:
+            TypeError: if `value` isn't an integer
+            ValueError: if `value` is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+        return value
+
+    @property
+    def height(self):
+        """property to retrive value of `height`"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """property setter method
+        Args:
+            value (int): value to be set
+        Raises:
+            TypeError: if `value` isn't an integer
+            ValueError: if `value` is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
+        return value
