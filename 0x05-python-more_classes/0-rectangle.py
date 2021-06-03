957e855f333f04cea3861bc7808a010766f3f034
Criptograma <81261737+Criptograma-1@users.noreply.github.com>
2021-06-02 22:38:51 -0300

    Fix task 0

--- 0x05-python-more_classes/0-rectangle.py
+++ 0x05-python-more_classes/0-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
             TypeError: if `value` isn't an integer
             ValueError: if `value` is less than 0
         """
-        if not isinstance(value, int):
+        if (type(value) is not int):
             raise TypeError("width must be an integer")
         if (value < 0):
             raise ValueError("width must be >= 0")
@@ -50,7 +50,7 @@ class Rectangle:
             TypeError: if `value` isn't an integer
             ValueError: if `value` is less than 0
         """
-        if not isinstance(value, int):
+        if (type(value) is not int):
             raise TypeError("height must be an integer")
         if (value < 0):
             raise ValueError("height must be >= 0")
