9196875440cb26423d085a033acc83aa8bdd65e3
Criptograma <81261737+Criptograma-1@users.noreply.github.com>
2021-06-02 22:56:16 -0300

    Fix PEP8

--- 0x05-python-more_classes/0-rectangle.py
+++ 0x05-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines class Rectangle"""
 
+
 class Rectangle:
     """Define the variable or attribute in the principal method"""
     def __init__(self, width=0, height=0):
