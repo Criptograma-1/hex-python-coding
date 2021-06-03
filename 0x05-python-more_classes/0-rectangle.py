18ddc8cdaed4f83110df142f73584070efca9310
Criptograma-1 <andresa.info@gmail.com>
2021-06-03 01:48:14 +0000

    Fix task 0 again.2

--- 0x05-python-more_classes/0-rectangle.py
+++ 0x05-python-more_classes/0-rectangle.py
@@ -12,6 +12,14 @@ class Rectangle:
         Note:
             ``Args`` section don't include `self` parameter
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if (width < 0):
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if (height < 0):
+            raise ValueError("height must be >= 0")
         self.__height = height
         self.__width = width
 
