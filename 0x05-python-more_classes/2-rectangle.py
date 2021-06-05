4441a4103276b065d16de1b5ecc1606898ece9e3
Criptograma-1 <andresa.info@gmail.com>
2021-06-05 20:30:52 +0000

    Fix task 2

--- 0x05-python-more_classes/2-rectangle.py
+++ 0x05-python-more_classes/2-rectangle.py
@@ -69,4 +69,4 @@ class Rectangle:
 
     def __repr__(self):
         """Returns string representation of rectangle"""
-        return "Rectangle(" + str(self.__width) + ", " + str(self.__height) + ")"
+        return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
