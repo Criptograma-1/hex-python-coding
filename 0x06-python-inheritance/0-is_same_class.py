27a40beabf3a5904de44b049ed423b3f5ab229b9
Criptograma <81261737+Criptograma-1@users.noreply.github.com>
2021-06-19 12:23:46 -0300

    Documented module

--- 0x06-python-inheritance/0-is_same_class.py
+++ 0x06-python-inheritance/0-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+defines class is_same_class
+"""
+
 
 def is_same_class(obj, a_class):
-
+    """returns true if the object is exactly an instance of the specified class"""
     return (isinstance(obj, a_class));
