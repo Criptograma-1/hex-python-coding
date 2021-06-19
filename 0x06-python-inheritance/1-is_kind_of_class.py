8eabf70c12690dd77f95127de43bd4ff591a0e9d
Criptograma-1 <andresa.info@gmail.com>
2021-06-19 15:55:32 +0000

    Add task 1

---
+++ 0x06-python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Prototype is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    if isinstance(obj, a_class):
+        """checks if obj belong to a_class"""
+        return True
+    return False
