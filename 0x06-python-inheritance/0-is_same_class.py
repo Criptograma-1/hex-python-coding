7c188a0bfdeb582a844a1b8f9e3d736db7795095
Criptograma <81261737+Criptograma-1@users.noreply.github.com>
2021-06-19 12:29:07 -0300

    Fix code

--- 0x06-python-inheritance/0-is_same_class.py
+++ 0x06-python-inheritance/0-is_same_class.py
@@ -6,4 +6,4 @@ defines class is_same_class
 
 def is_same_class(obj, a_class):
     """returns true if the object is exactly an instance of the specified class"""
-    return (isinstance(obj, a_class));
+    return (isinstance(obj, a_class))
