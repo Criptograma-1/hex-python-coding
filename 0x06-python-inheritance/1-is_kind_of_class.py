7dc514761afb19b7dcaae7717c82932d4dfa57ee
Criptograma <81261737+Criptograma-1@users.noreply.github.com>
2021-06-19 12:56:46 -0300

    Fix documentation

--- 0x06-python-inheritance/1-is_kind_of_class.py
+++ 0x06-python-inheritance/1-is_kind_of_class.py
@@ -5,6 +5,7 @@ Prototype is_kind_of_class
 
 
 def is_kind_of_class(obj, a_class):
+    """Defines function"""
     if isinstance(obj, a_class):
         """checks if obj belong to a_class"""
         return True
