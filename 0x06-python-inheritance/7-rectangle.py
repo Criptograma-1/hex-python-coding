c09ced8e901e8d2556cc1885f4a946b49e1cb7be
Criptograma <81261737+Criptograma-1@users.noreply.github.com>
2021-06-20 02:29:40 -0300

    Fix conditional in init

--- 0x06-python-inheritance/7-rectangle.py
+++ 0x06-python-inheritance/7-rectangle.py
@@ -10,10 +10,10 @@ class Rectangle(BaseGeometry):
     """Define the variable or attribute"""
     def __init__(self, width, height):
         """Validates the values with integer_validator"""
-        if self.integer_validator("width", width):
-            self.__width = width
-        if self.integer_validator("height", height):
-            self.__height = heigth
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = heigth
 
     def area(self):
         """Return area of a rectangle"""
