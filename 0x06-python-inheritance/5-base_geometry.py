18b04aecd45f4178059792811f302319e5cec15c
Criptograma <81261737+Criptograma-1@users.noreply.github.com>
2021-06-20 01:51:08 -0300

    Fix line 18
    'Type' to 'type'

--- 0x06-python-inheritance/5-base_geometry.py
+++ 0x06-python-inheritance/5-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
         """if `value` isn't a integer than raise a TypeError
            if `value` is less or equal to 0 raise a ValueError
         """
-        if Type(value) is not int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))
