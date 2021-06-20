78d2ddd697f65e23cad3b7a3e29d7985f67ee463
Criptograma <81261737+Criptograma-1@users.noreply.github.com>
2021-06-20 01:28:43 -0300

    Add task 4

---
+++ 0x06-python-inheritance/4-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    def area(self):
+        """raises an Exception"""
+        raise area
