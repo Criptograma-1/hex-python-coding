16211bffa3e2a34ebe16d1caa4809dcdf17425ba
Criptograma-1 <andresa.info@gmail.com>
2021-08-22 19:19:26 +0000

    Add task 3

---
+++ 0x07-python-input_output/3-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines function save_to_json_file"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an Object to a text file, using a JSON representation.
+    Args:
+        my_obj: string  that will be converted to JSON
+        filename: name of the file where the object will be saved
+    Return:
+        JSON representation of a string
+    """
+    return json.dump(my_obj, filename)
