d49042b6cda7f520c5e25404b255f6a60cdbf1ee
Criptograma-1 <andresa.info@gmail.com>
2021-08-22 18:09:28 +0000

    Documenting module

--- 0x07-python-input_output/0-append_write.py
+++ 0x07-python-input_output/0-append_write.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file and returns the number of characters added.
+    Args:
+        filename (str): Name of the file to be opened/created
+        text (str): Text that will be inserted into the file
+    Returns:
+        Number of characters added into the file
+    """
     with open(filename, 'a') as f:
         return(f.write(text))
