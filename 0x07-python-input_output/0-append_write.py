662e0b33813bd4f888b7379a9742607424d43519
Criptograma-1 <andresa.info@gmail.com>
2021-08-22 18:16:17 +0000

    fix PEP8

--- 0x07-python-input_output/0-append_write.py
+++ 0x07-python-input_output/0-append_write.py
@@ -4,7 +4,7 @@
 
 def append_write(filename="", text=""):
     """
-    Appends a string at the end of a text file and returns 
+    Appends a string at the end of a text file and returns
     the number of characters added.
 
     Args:
