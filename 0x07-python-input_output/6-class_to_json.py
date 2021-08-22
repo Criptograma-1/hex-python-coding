8f74f6d2420c5cd974c54aba4bbb8b26ba2a54e4
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-08-22 17:42:54 -0300

    Update 6-class_to_json.py

--- 0x07-python-input_output/6-class_to_json.py
+++ 0x07-python-input_output/6-class_to_json.py
@@ -4,10 +4,12 @@
 
 def class_to_json(obj):
     """
-    Convert class to dict
+    Appends a string at the end of a text file and returns
+    the number of characters added.
     Args:
-        obj: object from a class
-    Return:
-        Object turn to dictionary
+        filename (str): Name of the file to be opened/created
+        text (str): Text that will be inserted into the file
+    Returns:
+        Number of characters added into the file
     """
     return obj = obj.__dict__
