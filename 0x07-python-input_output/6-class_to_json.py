75f7e9568403d38b19ef3930b15c92194c7c2787
Criptograma-1 <andresa.info@gmail.com>
2021-08-22 20:36:18 +0000

    Add task 6

--- 0x07-python-input_output/6-class_to_json.py
+++ 0x07-python-input_output/6-class_to_json.py
@@ -3,5 +3,11 @@
 
 
 def class_to_json(obj):
-    """Convert class to dict"""
+    """
+    Convert class to dict
+    Args:
+        obj: object from a class
+    Return:
+        Object turn to dictionary
+    """
     return obj = obj.__dict__
