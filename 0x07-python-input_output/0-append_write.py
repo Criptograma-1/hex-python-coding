18d174e07cd4a00a7794934dfc71656adb6d784b
Criptograma-1 <andresa.info@gmail.com>
2021-08-22 18:12:10 +0000

    Documenting module

--- 0x07-python-input_output/0-append_write.py
+++ 0x07-python-input_output/0-append_write.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines the function"""
+
 def append_write(filename="", text=""):
     """
     Appends a string at the end of a text file and returns the number of characters added.
