fe03370b27403649c0bee7d2b26cabe80f3be6f2
Criptograma-1 <andresa.info@gmail.com>
2021-08-22 19:25:24 +0000

    Fix code to use with statement

--- 0x07-python-input_output/3-save_to_json_file.py
+++ 0x07-python-input_output/3-save_to_json_file.py
@@ -13,4 +13,5 @@ def save_to_json_file(my_obj, filename):
     Return:
         JSON representation of a string
     """
-    return json.dump(my_obj, filename)
+    with open(filename, 'w') as f:
+        return json.dump(my_obj, f)
