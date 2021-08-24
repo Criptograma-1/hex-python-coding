841bce728b849ffab47da63930b5d27c71f46f01
Criptograma-1 <andresa.info@gmail.com>
2021-08-24 19:52:55 +0000

    Add task 14

--- 0x08-python-almost_a_circle/models/base.py
+++ 0x08-python-almost_a_circle/models/base.py
@@ -14,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
