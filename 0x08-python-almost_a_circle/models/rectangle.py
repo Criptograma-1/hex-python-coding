047d7116904c8d3a018eb9a3793c896bb3613582
Criptograma-1 <andresa.info@gmail.com>
2021-08-24 16:01:23 +0000

    Fix method area

--- 0x08-python-almost_a_circle/models/rectangle.py
+++ 0x08-python-almost_a_circle/models/rectangle.py
@@ -18,7 +18,7 @@ class Rectangle(Base):
 
     def area(self):
         """Return area of the rectangle"""
-        return (self.__width * Self.__height)
+        return (self.__width ** Self.__height)
 
     @property
     def width(self):
