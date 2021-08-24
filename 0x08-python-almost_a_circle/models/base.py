0ceaa93290edda7aa1b91d6948b3c1a2ef13e892
Criptograma-1 <andresa.info@gmail.com>
2021-08-24 20:01:08 +0000

    Add json import

--- 0x08-python-almost_a_circle/models/base.py
+++ 0x08-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Create class Base"""
 
+import json
+
 
 class Base:
     """A base class for handle id"""
@@ -18,7 +20,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) < 1:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
