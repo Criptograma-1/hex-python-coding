970a5b59abe99d660d21b419a0e73a5aac365e93
Criptograma-1 <andresa.info@gmail.com>
2021-08-24 20:28:34 +0000

    Add task 17

--- 0x08-python-almost_a_circle/models/base.py
+++ 0x08-python-almost_a_circle/models/base.py
@@ -41,3 +41,13 @@ class Base:
         if json_string is None or len(json_string) < 1:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        if cls.__name__ == 'Rectangle':
+            obj = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            obj = cls(1)
+        obj.update(**dictionary)
+        return obj
