7fa52b8b1d7087253437358e6e87f85f1857cac2
Criptograma-1 <andresa.info@gmail.com>
2021-08-24 19:55:40 +0000

    Fix 'to_json_string is not a static method'

--- 0x08-python-almost_a_circle/models/base.py
+++ 0x08-python-almost_a_circle/models/base.py
@@ -15,6 +15,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @stacticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None:
