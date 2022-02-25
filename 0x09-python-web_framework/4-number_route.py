ca1bec01552e0e66baa8cdf3854549136796ddbb
Criptograma-1 <andresa.infp@gmail.com>
2022-02-25 18:24:34 +0000

    Add task 4

---
+++ 0x09-python-web_framework/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Script to starts a Flask web Application"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def C(text):
+    return ("C " + text.replace("_", " "))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text="is cool"):
+    return ("Python " + text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def is_number(n):
+    return ("{} is a number".format(n))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
