be4072e1e83ff4c24ec1256957d5c7676795ab60
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-02-25 14:38:48 -0300

    Fix PEP8

--- 0x09-python-web_framework/2-c_route.py
+++ 0x09-python-web_framework/2-c_route.py
@@ -3,7 +3,7 @@
 """Script to starts a Flask web Application"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes=False
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
