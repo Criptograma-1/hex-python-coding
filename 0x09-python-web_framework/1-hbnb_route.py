f37d5b709f566f87c1f64bcb10eafeafd9aa0ffb
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-02-25 14:31:50 -0300

    Fix PEP8

--- 0x09-python-web_framework/1-hbnb_route.py
+++ 0x09-python-web_framework/1-hbnb_route.py
@@ -3,11 +3,13 @@
 """Script to starts a Flask web Application"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes=False
+app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello_HBNB():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def HBNB():
