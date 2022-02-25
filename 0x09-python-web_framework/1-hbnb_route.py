616e641d22fdc0fd995f0eb81de7ea9f1817b125
Criptograma-1 <andresa.infp@gmail.com>
2022-02-25 17:29:07 +0000

    Add task 1

--- 0x09-python-web_framework/1-hbnb_route.py
+++ 0x09-python-web_framework/1-hbnb_route.py
@@ -8,3 +8,10 @@ app.url_map.strict_slashes=False
 @app.route('/')
 def hello_HBNB():
     return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def HBNB():
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
