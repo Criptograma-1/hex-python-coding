420bb547a6ae1315429390f530f09860daf18835
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-03-09 15:34:00 -0300

    Fix PEP8

--- 0x0A-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0A-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,9 +6,12 @@ from sys import argv
 
 if __name__ == '__main__':
     """Access DB and get states"""
-    db=MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-    c=db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE BINARY %(arg)s", {'arg': argv[4]})
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %(arg)s", {'arg': argv[4]}
+    )
     rows = c.fetchall()
     if rows is not None:
         for row in rows:
