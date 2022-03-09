db28d31313e3a094fc9c372cbde15db2ed5a0db3
Criptograma <andresa.info>
2022-03-09 17:54:32 +0000

    Add task 0

---
+++ 0x0A-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """Access DB and get states"""
+    db=MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    c=db.cursor()
+    c.execute("SELECT * FROM states")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
