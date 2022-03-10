48a4d8dd591f81c443f7584ea99600e4e58b5fba
Criptograma <andresa.info>
2022-03-10 18:36:16 +0000

    Add task 20

---
+++ 0x0A-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument from the database"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(name = argv[4]).first()
+
+    if state is not None:
+        print(str(state.id))
+    else:
+        print('Not found')
+
+    session.close()
