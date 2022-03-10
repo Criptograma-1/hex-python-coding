4cc5cccc7b77bd59f0abf8767213dc306f65bf45
Criptograma <andresa.info>
2022-03-10 17:57:46 +0000

    Add task 6

---
+++ 0x0A-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""contains the class definition of a State and an instance Base = declarative_base()"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class definition of a State"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
