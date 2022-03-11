361070b9251de41495690f69f6c6ef29b8fe55d7
Criptograma <andresa.info>
2022-03-11 18:54:38 +0000

    Add task 17

---
+++ 0x0A-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" List all state objects using sqlalchemy """
+
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm.session import sessionmaker, Session
+from sqlalchemy import create_engine
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).order_by(City.id)
+    for city in cities:
+        print('{}: {} -> {}'.format(city.id, city.name, city.state.name))
