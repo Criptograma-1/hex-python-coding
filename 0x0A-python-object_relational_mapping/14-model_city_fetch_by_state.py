b73b449e8488edd21ce6db72cd27388dee4fc7a1
Criptograma <andresa.info>
2022-03-11 18:06:05 +0000

    Add task 14

---
+++ 0x0A-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that adds the State object 'Louisiana' to the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+from model_city import City
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_cities = session.query(State, City).filter(State.id == City.state_id)\
+        .order_by(City.id)
+
+    for state, city in states_cities:
+        print('{}: ({}) {}'.format(state.name, city.id, city.name))
---
+++ 0x0A-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""contains the class definition of a State and an instance Base = declarative_base()"""
+
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """class definition of a City"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
