5ae54c84912e640b67d329d21de43822da2aecf1
Criptograma <andresa.info>
2022-03-11 17:47:40 +0000

    Add task 13

---
+++ 0x0A-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that adds the State object 'Louisiana' to the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name.like('%a%'))
+    state.delete(synchronize_session=False)
+    session.commit()
+    session.close()
